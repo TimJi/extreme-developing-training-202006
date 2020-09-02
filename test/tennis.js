39d24c2da32d38d5e733012f61f6a5287564d6c5
Tim <timji0210@gmail.com>
2020-09-02 13:17:35 +0800

    day14

---
+++ test/tennis.js
@@ -0,0 +1,85 @@
+export class Tennis {
+  _firstPlayerScoreTimes = 0
+  _scoreMapping = {
+    1: "Fifteen",
+    2: "Thirty",
+    3: "Forty",
+    0: "Love",
+  }
+  _secondPlayerScoreTimes = 0
+  _firstPlayerName
+  _secondPlayerName
+
+  constructor(firstPlayerName, secondPlayerName) {
+    this._firstPlayerName = firstPlayerName
+    this._secondPlayerName = secondPlayerName
+  }
+
+  score() {
+    return this.isScoreDiff()
+      ? this.isMatchPoint()
+        ? this.isAdv()
+          ? this.advScore()
+          : this.winScore()
+        : this.diffScore()
+      : this.isMatchPoint()
+      ? this.deuceScore()
+      : this.sameScore()
+  }
+
+  winScore() {
+    return this.advPlayer() + " Win"
+  }
+
+  advScore() {
+    return this.advPlayer() + " Adv"
+  }
+
+  advPlayer() {
+    return this._firstPlayerScoreTimes > this._secondPlayerScoreTimes
+      ? this._firstPlayerName
+      : this._secondPlayerName
+  }
+
+  isAdv() {
+    return (
+      Math.abs(this._firstPlayerScoreTimes - this._secondPlayerScoreTimes) === 1
+    )
+  }
+
+  isScoreSame() {
+    return this._firstPlayerScoreTimes === this._secondPlayerScoreTimes
+  }
+
+  deuceScore() {
+    return "Deuce"
+  }
+
+  isMatchPoint() {
+    return this._firstPlayerScoreTimes > 2 && this._secondPlayerScoreTimes > 2
+  }
+
+  sameScore() {
+    return this._scoreMapping[this._firstPlayerScoreTimes] + " All"
+  }
+
+  diffScore() {
+    return (
+      this._scoreMapping[this._firstPlayerScoreTimes] +
+      " " +
+      this._scoreMapping[this._secondPlayerScoreTimes]
+    )
+  }
+
+  isScoreDiff() {
+    return this._firstPlayerScoreTimes !== this._secondPlayerScoreTimes
+  }
+
+  firstPlayerScore() {
+    this._firstPlayerScoreTimes++
+  }
+
+  secondPlayerScore() {
+    this._secondPlayerScoreTimes++
+  }
+}
